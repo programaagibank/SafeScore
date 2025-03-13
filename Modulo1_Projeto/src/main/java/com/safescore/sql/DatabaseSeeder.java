c92d30f9221c942edcd1e9d84f1e77a3dbcbbc8e
anscarpetta <angeloscarpetta2020@gmail.com>
2025-03-13 11:58:33 -0300

    Patrimonio + Historico de Credito Aleatorios.

--- Modulo1_Projeto/src/main/java/com/safescore/sql/DatabaseSeeder.java
+++ Modulo1_Projeto/src/main/java/com/safescore/sql/DatabaseSeeder.java
@@ -5,8 +5,9 @@ import net.datafaker.Faker;
 import java.util.*;
 
 public class DatabaseSeeder {
+  static Faker faker = new Faker(Locale.of("pt", "BR"));
+
   public static void test() {
-    Faker faker = new Faker(Locale.of("pt", "BR"));
 
     //usuario
     System.out.println(faker.name().name());
@@ -29,24 +30,78 @@ public class DatabaseSeeder {
             "Pós-doutorado"
     };
 
-    double[] probabilidades = calculadorProbabilidade(escolaridades.length, 4);
-    double random = Math.random();
-    double cumulative = 0.0;
+    System.out.println(calculadorProbabilidade(4, escolaridades));
 
-    for (int i = 0; i < escolaridades.length; i++) {
-      cumulative += probabilidades[i];
-      if (random <= cumulative) {
-        System.out.println("Escolaridade: " + escolaridades[i]);
-        break;
-      }
+    //estado civil
+    String[] estadoCivil =
+            {"Solteiro", "Casado", " Viuvo", "Divorciado"};
+    System.out.println(dadoAleatorio(estadoCivil));
+
+    //historicoCredito
+    int parcelasAPagar = faker.number().numberBetween(0, 100);
+    int valorCredito = faker.number().numberBetween(1, 100);
+    for (int i = 0; i < faker.number().numberBetween(0, 6); i++) {
+      valorCredito *= 10;
     }
+    ;
+    int parcela = valorCredito / parcelasAPagar;
+    int parcelasRestantes = faker.number().numberBetween(0, parcelasAPagar);
+    int creditorestante = parcela * (parcelasAPagar - parcelasRestantes);
+    Boolean estaInandinplente = faker.bool().bool();
+    int mesesAtrasado = estaInandinplente ? Math.min(faker.number().numberBetween(0, 100), faker.number().numberBetween(0, 100)) : 0;
+    System.out.println("ParcelaRestante: " + parcelasRestantes);
+    System.out.println("Parcela: " + parcela);
+    System.out.println("MesesAtrasado: " + mesesAtrasado);
+    System.out.println("EstaInadinplente: " + estaInandinplente);
+    System.out.println("CreditoRestante: " + creditorestante);
+    System.out.println("Valor Total: " + valorCredito);
+    //e cpf
+
+    //patrimonio
+    //1/4
+
+    int nivelRenda = faker.number().numberBetween(1, 10);
+    int saldo =  sorteadorDeRenda(nivelRenda);
+    System.out.println("Investimentos: " + (nivelRenda < 8 ? 0 : sorteadorDeRenda(nivelRenda)));
+    System.out.println("Bens: " + (sorteadorDeRenda(nivelRenda) + saldo));
+    System.out.println("Saldo: " + saldo);
+
+    //ecpf
+
 
   }
 
-  public static double[] calculadorProbabilidade(int tamanhoOpcoes, int opcaoComum) {
-    double[] probabilidades = new double[tamanhoOpcoes];
-    int distanciaMaxima = Math.max(opcaoComum, tamanhoOpcoes - opcaoComum - 1);
-    for (int i = 0; i < tamanhoOpcoes; i++) {
+
+  public static int sorteadorDeRenda(int nivelRenda) {
+    return switch (nivelRenda) {
+      case 1, 2, 3, 4, 5, 6, 7 -> baixoValorMonetario();
+      case 8, 9 -> medioValorMonetario();
+      case 10 -> altoValorMonetario();
+      default -> 0;
+    };
+  }
+
+  public static int baixoValorMonetario() {
+    return Math.min(faker.number().numberBetween(0, 100000), faker.number().numberBetween(0, 100000));
+  }
+
+  public static int medioValorMonetario() {
+    return Math.min(faker.number().numberBetween(0, 700000), faker.number().numberBetween(0, 700000));
+  }
+
+  public static int altoValorMonetario() {
+    return Math.min(faker.number().numberBetween(0, 10000000), faker.number().numberBetween(0, 10000000));
+  }
+
+
+  public static String dadoAleatorio(String[] opcoes) {
+    return opcoes[faker.number().numberBetween(0, opcoes.length - 1)];
+  }
+
+  public static String calculadorProbabilidade(int opcaoComum, String[] opcoes) {
+    double[] probabilidades = new double[opcoes.length];
+    int distanciaMaxima = Math.max(opcaoComum, opcoes.length - opcaoComum - 1);
+    for (int i = 0; i < opcoes.length; i++) {
       int distancia = Math.abs(i - opcaoComum);
       probabilidades[i] = distanciaMaxima - distancia + 1;
     }
@@ -55,12 +110,20 @@ public class DatabaseSeeder {
     for (double peso : probabilidades) {
       total += peso;
     }
-    for (int i = 0; i < tamanhoOpcoes; i++) {
+    for (int i = 0; i < opcoes.length; i++) {
       probabilidades[i] /= total;
     }
-    return probabilidades;
-  }
+    double random = Math.random();
+    double acc = 0.0;
 
+    for (int i = 0; i < opcoes.length; i++) {
+      acc += probabilidades[i];
+      if (random <= acc) {
+        return opcoes[i];
+      }
+    }
+    return null;
+  }
 
   public static void main(String[] args) {
     test();
